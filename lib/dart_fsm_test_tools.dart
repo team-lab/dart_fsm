fee59ded195ca4d788f1166a867a8d2400084bba
SoutaTanaka <tanakasouta@team-lab.com>
2024-03-15 17:47:22 +0900

    chore

--- lib/dart_fsm_test_tools.dart
+++ lib/dart_fsm_test_tools.dart
@@ -1,5 +1,4 @@
-library;
 
+export 'src/tester/mock_state_machine.dart';
 export 'src/tester/state_machine_tester.dart';
 export 'src/tester/tester_state_machine.dart';
-export 'src/tester/mock_state_machine.dart';
